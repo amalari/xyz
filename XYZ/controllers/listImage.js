8fc14794e19af568780e384c7b4908791ea3a73e
unknown <cobian.wae@gmail.com>
2015-11-10 18:22:23 +0700

    upload

--- XYZ/controllers/listImage.js
+++ XYZ/controllers/listImage.js
@@ -1,26 +1,54 @@
 var fs = require('fs');
 var path = require('path');
+var Multipart = require('./../core/multipart/index.js');
+var FileManager = require('./../core/file-manager/index.js');
 
 TinyvisonController = {
 	registerRoutes : function(app){
 		console.log('masuk tinyvison');
 		app.get('/list/image', this.list);
+		app.post('/list/image', this.post);
 	},
 	list : function(req, res){
 		var result = {};
 		result.data = [];
 		console.log("masuk sini ga?");
 		fs.readdir('public/images', function(err, files){
-			var obj = {};
-			console.log(files);
-			var x = '/images/' + files[0];
-			var y = '/images/' + files[1];
-			var z = files[1];
-			obj.imageUrl = x;
-			obj.name = z;
-			obj.value = y
-			result.data.push(obj);
+			files.forEach(function(val, i){
+					var obj = {};
+					obj.imageUrl = '/images/' + val;
+					obj.name = val;
+					obj.value = '/images/' + val;
+					result.data.push(obj);
+			})
 			res.json(result);
+		})
+	},
+	post : function(req, res){
+		var formMultipart = new Multipart({
+			uploadDir : __dirname + '/../public/images/',
+			allowedMimeTypes : ['image/jpeg', 'image/png', 'image/gif' ]
+		});
+		var formFileManager = new FileManager({
+			dir : __dirname + '/../public/images',
+			baseUrl : '/images'
+		});
+		formMultipart.parseAndSaveFiles(req, function(data){
+			// var newData = {};
+			// newData.client_id = req.query.clientId;
+			// for(var key in data){
+			// 	if(key.indexOf("file") > -1){
+			// 		newData[key] = formFileManager.getUrl(data[key])
+			// 	} else {
+			// 		newData[key] = data[key];
+			// 	}
+			// };
+			// var result = ProjectRequestViewModel.save(newData);
+			// ProjectRequest.save(result)
+			// .then(function(){
+			// 	res.render("finish-form")
+			// })
+			res.send({success:true});
 		})
 	}
 };
